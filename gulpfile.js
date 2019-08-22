4c2b033840d290db58fea9b7067a45243e03cfe9
Alex Hansen <alxo@cryptokitties.city>
2019-08-21 23:53:54 -0700

    minimize gulpfile to 1 line

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,1 @@
-var gulp = require("gulp");
-var watch = require("gulp-watch");
-var babel = require("gulp-babel");
-
-gulp.task("default", defaultTask);
-
-function defaultTask(done) {
-  // place code for your default task here
-  return watch("src/*", { ignoreInitial: false })
-    .pipe(babel())
-    .pipe(gulp.dest("lib"));
-  done();
-}
+require("gulp").task("default", () => require("gulp-watch")("src/*", { ignoreInitial: false }).pipe(require("gulp-babel")()).pipe(require("gulp").dest("lib")));
