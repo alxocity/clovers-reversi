ecb5a4e14a355d9b67c615e28273bb1a65a00f1f
Alex Hansen <alexhansen@pointstire.com>
2019-08-22 14:15:16 -0700

    add url to output and print avg ms per 1000 games

--- src/miner.js
+++ src/miner.js
@@ -26,68 +26,62 @@ class Miner {
   }
 
   mine() {
-      this.date = new Date()
-      if (!this.stopMining) {
-        // if (this.totalGamesMined%1000 == 0) fs.appendFileSync(this.log, 'avg:' + (this.date.getTime() - this.started.getTime()) / (this.totalGamesMined / 1000))
-        if (this.totalGamesMined == 0) fs.appendFileSync(this.log, 'avg:' + (this.date.getTime() - this.started.getTime()) / (this.totalGamesMined / 1000) + '\r\n')
-        if (this.totalGamesMined == 1) fs.appendFileSync(this.log, 'avg:' + (this.date.getTime() - this.started.getTime()) / (this.totalGamesMined / 1000) + '\r\n')
+    this.date = new Date()
+    if (!this.stopMining) {
+      if (this.totalGamesMined % 1000 == 0) fs.appendFileSync(this.log, 'avg:' + (this.date.getTime() - this.started.getTime()) / (this.totalGamesMined / 1000))
 
-        this.tempGamesMined++
-        this.totalGamesMined++
-        this.reversi.mine()
-        if(this.reversi.symmetrical) {
-          if (this.reversi.RotSym) {
-            this.RotSym++
-            fs.appendFileSync(this.log, 'RotSym' + '\r\n');
-          }
-          if (this.reversi.Y0Sym) {
-            this.Y0Sym++
-            fs.appendFileSync(this.log, 'Y0Sym' + '\r\n');
-          }
-          if (this.reversi.X0Sym) {
-            this.X0Sym++
-            fs.appendFileSync(this.log, 'X0Sym' + '\r\n');
-          }
-          if (this.reversi.XYSym) {
-            this.XYSym++
-            fs.appendFileSync(this.log, 'XYSym' + '\r\n');
-          }
-          if (this.reversi.XnYSym) {
-            this.XnYSym++
-            fs.appendFileSync(this.log, 'XnYSym' + '\r\n');
-          }
-          this.totalCloversFound++
-          this.allGamesMined.push(this.tempGamesMined)
-          fs.appendFileSync(this.log, 'time: ' + (this.date.getTime() - this.lastTime) + '\r\n');
-          this.lastTime = this.date.getTime()
-          fs.appendFileSync(this.log, 'current: ' + this.tempGamesMined + '\r\n');
-          fs.appendFileSync(this.log, 'average: ' + (this.totalGamesMined / this.totalCloversFound) + '\r\n');
-          fs.appendFileSync(this.log, 'total: ' + this.totalGamesMined + '\r\n');
-          this.tempGamesMined = 0
-
-          fs.appendFileSync(this.log, this.reversi.movesString + '\r\n');
-
-          this.reversi.playGameMovesString(this.reversi.movesString);
-          this.reversi.translateToF5Version();
-          fs.appendFileSync(this.log, this.reversi.movesString + '\r\n');
-
-          this.reversi.playGameMovesString(this.reversi.movesString);
-          this.reversi.translateToD3Version();
-          fs.appendFileSync(this.log, this.reversi.movesString + '\r\n');
-
-          this.reversi.playGameMovesString(this.reversi.movesString);
-          this.reversi.translateToE6Version();
-          fs.appendFileSync(this.log, this.reversi.movesString + '\r\n');
-
-          this.reversi.playGameMovesString(this.reversi.movesString);
-          this.reversi.translateToC4Version();
-          fs.appendFileSync(this.log, this.reversi.movesString + '\r\n');
-
-          this.reversi.makeVisualBoard();
-          for (let i = 0; i < this.reversi.BOARDDIM; i++)
-            fs.appendFileSync(this.log, this.reversi.visualBoard[i].join('') + '\r\n');
+      this.tempGamesMined++
+      this.totalGamesMined++
+      this.reversi.mine()
+      if (this.reversi.symmetrical) {
+        if (this.reversi.RotSym) {
+          this.RotSym++
+          fs.appendFileSync(this.log, 'RotSym' + '\r\n');
         }
+        if (this.reversi.Y0Sym) {
+          this.Y0Sym++
+          fs.appendFileSync(this.log, 'Y0Sym' + '\r\n');
+        }
+        if (this.reversi.X0Sym) {
+          this.X0Sym++
+          fs.appendFileSync(this.log, 'X0Sym' + '\r\n');
+        }
+        if (this.reversi.XYSym) {
+          this.XYSym++
+          fs.appendFileSync(this.log, 'XYSym' + '\r\n');
+        }
+        if (this.reversi.XnYSym) {
+          this.XnYSym++
+          fs.appendFileSync(this.log, 'XnYSym' + '\r\n');
+        }
+        this.totalCloversFound++
+        this.allGamesMined.push(this.tempGamesMined)
+        fs.appendFileSync(this.log, 'time: ' + (this.date.getTime() - this.lastTime) + '\r\n');
+        this.lastTime = this.date.getTime()
+        fs.appendFileSync(this.log, 'current: ' + this.tempGamesMined + '\r\n');
+        fs.appendFileSync(this.log, 'average: ' + (this.totalGamesMined / this.totalCloversFound) + '\r\n');
+        fs.appendFileSync(this.log, 'total: ' + this.totalGamesMined + '\r\n');
+        this.tempGamesMined = 0
+
+        fs.appendFileSync(this.log, `https://clovers.network/account/basket?pick=${this.reversi.movesString}\r\n`);
+
+        this.reversi.playGameMovesString(this.reversi.movesString);
+        this.reversi.translateToF5Version();
+        fs.appendFileSync(this.log, `https://clovers.network/account/basket?pick=${this.reversi.movesString}\r\n`);
+
+        this.reversi.playGameMovesString(this.reversi.movesString);
+        this.reversi.translateToD3Version();
+        fs.appendFileSync(this.log, `https://clovers.network/account/basket?pick=${this.reversi.movesString}\r\n`);
+
+        this.reversi.playGameMovesString(this.reversi.movesString);
+        this.reversi.translateToE6Version();
+        fs.appendFileSync(this.log, `https://clovers.network/account/basket?pick=${this.reversi.movesString}\r\n`);
+
+        this.reversi.makeVisualBoard();
+        for (let i = 0; i < this.reversi.BOARDDIM; i++)
+          fs.appendFileSync(this.log, this.reversi.visualBoard[i].join('') + '\r\n');
       }
+    }
 
   }
 }
